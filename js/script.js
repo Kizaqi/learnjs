c490198f724102f950c3bebd8327788223aa0e52
Mykhailo Luzhetskyi <luzeckijmihajlo0@gmail.com>
2023-02-09 18:48:18 +0200

    task - function

--- js/script.js
+++ js/script.js
@@ -73,6 +73,71 @@
 // }
 // console.log(personalMovieDB);
 
+//Task 3
+let numberOfFilms;
+
+function start() {
+ numberOfFilms = +prompt("Скільки фільмів ви подивились", "");
+
+ while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+  numberOfFilms = +prompt("Скільки фільмів ви подивились", "");
+ }
+}
+// start();
+
+const personalMovieDB = {
+ count: numberOfFilms,
+ movies: {},
+ actors: {},
+ genres: [],
+ privat: false,
+};
+
+function rememer() {
+ for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+   b = prompt("На сколько оцените его?", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+   personalMovieDB.movies[a] = b;
+   console.log("done");
+  } else {
+   console.log("error");
+   i--;
+  }
+ }
+}
+// rememer();
+
+function deteckedPersonalLevel() {
+ if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+ } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+ } else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+ } else {
+  console.log("Произошла ошибка");
+ }
+ console.log(personalMovieDB);
+}
+// deteckedPersonalLevel();
+
+function showMyDB(hidden) {
+ if (!hidden) {
+  console.log(personalMovieDB);
+ }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+ for (let i = 1; i <= 3; i++) {
+  const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+  personalMovieDB.genres[i - 1] = genre;
+ }
+}
+writeYourGenres();
+
 // // if (num < 49) {
 // //   console.log("Error");
 // // } else if (num > 100) {
@@ -189,8 +254,8 @@
 
 //MEDODY
 
-const fruit = "Hellow world";
-console.log(fruit.slice(0, 4));
+// const fruit = "Hellow world";
+// console.log(fruit.slice(0, 4));
 
-const logg = "Luzhetskyi";
-console.log(logg.indexOf("e"));
+// const logg = "Luzhetskyi";
+// console.log(logg.indexOf("e"));
